7a4feafe4095ec2876e34feefdaa92697be3f6ea
Simo Dida <simodida383@gmail.com>
2022-06-24 16:02:28 +0100

    Preview Card

---
+++ src/components/image/image.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function Img() {
+    return (
+        <div>
+            Img
+        </div>
+    );
+}
+
+export default Img;
---
+++ src/components/insight/insight.jsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+function Insight() {
+    return (
+        <>
+            <div>Insight</div>
+        </>
+    );
+}
+export default Insight;
